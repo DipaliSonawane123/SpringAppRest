6113b783e25947a8670a6c1159477f0c94387c69
Dipali <dipssonawane123@gmail.com>
2022-08-27 19:16:04 +0530

    Modified:Use GET Request Method and pass name as path variable is modified

--- src/main/java/com/example/firstproject/controller/RestAPI.java
+++ src/main/java/com/example/firstproject/controller/RestAPI.java
@@ -1,21 +1,25 @@
 package com.example.firstproject.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestAPI {
-    @RequestMapping("/api")
+    @RequestMapping("/home")
     public String Hello (){
         return "<h1>Hello From BridgeLabz!!</h1>";
     }
     //Use GET Request Method and pass name as query parameter
     @GetMapping("/query")
-    public String getUsers(@RequestParam(value = "name", defaultValue = "Dipali") String name)
-    {
-        return "Hello! " + name + " From BridgeLabz!!";
+    public String getUsers(@RequestParam(value = "name", defaultValue = "Dipali") String name) {
+        return "<h1><font color=blue>Hello! " + name + " From BridgeLabz!!</font></h1>" +
+                "</br> <font color=green>Passing name as a parameter.</font>";
     }
+    //Use GET Request Method and pass name as path variable
+    @RequestMapping(value = {"/query/{name}"}, method = RequestMethod.GET)
+    public String greet(@PathVariable String name) {
+        return "<h1><font color=Black>Hello! " + name + " From BridgeLabz!!</font></h1>" +
+                "</br> <font color=Green>Passing name as a path variable.</font>";
+    }
+
 }
