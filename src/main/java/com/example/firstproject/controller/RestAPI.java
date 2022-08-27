fe9c175c73d2f9d429dc8a6c0bd11bf2bf934804
Dipali <dipssonawane123@gmail.com>
2022-08-27 19:37:29 +0530

    Modified:Use POST Request Method and pass first name and last name in the Body is modified

--- src/main/java/com/example/firstproject/controller/RestAPI.java
+++ src/main/java/com/example/firstproject/controller/RestAPI.java
@@ -1,7 +1,10 @@
 package com.example.firstproject.controller;
 
 
+import com.example.firstproject.model.FirstModel;
 import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
 
 @RestController
 public class RestAPI {
@@ -20,6 +23,11 @@ public class RestAPI {
     public String greet(@PathVariable String name) {
         return "<h1><font color=Black>Hello! " + name + " From BridgeLabz!!</font></h1>" +
                 "</br> <font color=Green>Passing name as a path variable.</font>";
+    }
+    //Use POST Request Method and pass first name and last name in the Body
+    @PostMapping("/post")
+    public String post(@RequestBody FirstModel  entity) {
+        return "Hello " + entity.getFirstName() + " "+  entity.getLastName() + "!";
     }
 
 }
